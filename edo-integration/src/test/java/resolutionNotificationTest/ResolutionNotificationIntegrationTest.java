69e9dcbb1e4e95c7adb9261dad13a718d3cdaed3
Гребенщиков Илья <bliz174@gmail.com>
2023-07-02 10:15:00 -0400

    added the GreenMail mock server and all dependency

--- edo-integration/src/test/java/resolutionNotificationTest/ResolutionNotificationIntegrationTest.java
+++ edo-integration/src/test/java/resolutionNotificationTest/ResolutionNotificationIntegrationTest.java
@@ -1,11 +1,11 @@
 package resolutionNotificationTest;
 
 import com.education.EdoIntegrationApplication;
+import com.icegreen.greenmail.configuration.GreenMailConfiguration;
 import com.icegreen.greenmail.util.GreenMail;
-import com.icegreen.greenmail.util.ServerSetupTest;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import com.icegreen.greenmail.util.ServerSetup;
+import jakarta.mail.internet.MimeMessage;
+import org.junit.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,7 +15,9 @@ import org.springframework.messaging.MessagingException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
-import jakarta.mail.internet.MimeMessage;
+
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -25,63 +27,57 @@ import static org.junit.Assert.assertEquals;
 @TestPropertySource(properties = {
         "spring.mail.host=localhost",
         "spring.mail.port=2525",
-        "spring.mail.username=user@example.com",
-        "spring.mail.password=password",
         "spring.mail.protocol=smtp",
-        "spring.mail.properties.mail.smtp.auth=true",
+        "spring.mail.properties.mail.smtp.auth=false",
         "spring.mail.properties.mail.smtp.starttls.enable=false",
         "spring.mail.test-connection=true"
 })
+
 public class ResolutionNotificationIntegrationTest {
 
-    private GreenMail greenMail;
+    private static GreenMail smtpServer;
 
     @Autowired
     private JavaMailSender javaMailSender;
 
-    @Before
-    public void startMailServer() {
-        greenMail = new GreenMail(ServerSetupTest.SMTP);
-        greenMail.start();
+    @BeforeClass
+    public static void setUp() {
+        smtpServer = new GreenMail(new ServerSetup(2525, null, "smtp"))
+                .withConfiguration(GreenMailConfiguration.aConfig().withDisabledAuthentication());
+        smtpServer.start();
     }
 
-    @After
-    public void stopMailServer() {
-        greenMail.stop();
+    @AfterClass
+    public static void tearDown() {
+        smtpServer.stop();
     }
+
 
     @Test
-    public void testEmailSending() throws MessagingException, javax.mail.MessagingException, jakarta.mail.MessagingException {
-//        // Wait for the email to be delivered and received by the mock SMTP server
-//        try {
-//            Thread.sleep(2000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
+    public void testEmailSending() throws MessagingException, jakarta.mail.MessagingException {
+//        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+//        MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true);
+//        messageHelper.setFrom("sender@example.com");
+//        messageHelper.setTo("recipient@example.com");
+//        messageHelper.setSubject("Test Email");
+//        messageHelper.setText("This is a test email");
 //
-//        // Retrieve the captured emails from the mock SMTP server
-//        MimeMessage[] receivedMessages = greenMail.getReceivedMessages();
-//
-//        // Assert the number of captured emails
-//        assertEquals(3, receivedMessages.length);
-//
-//        // Assert the content of each captured email
-//        for (MimeMessage message : receivedMessages) {
-//            assertEquals("Subject", message.getSubject());
-//            assertEquals("sender@example.com", message.getFrom()[0].toString());
-//            assertEquals("recipient@example.com", message.getAllRecipients()[0].toString());
-//        }
-        MimeMessage message = javaMailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message);
-        helper.setTo("recipient@example.com");
-        helper.setSubject("Test Email");
-        helper.setText("This is a test email");
+//        javaMailSender.send(mimeMessage);
 
-        javaMailSender.send(message);
+        // Wait for the email to be delivered and received by the mock SMTP server
+        smtpServer.waitForIncomingEmail(2000, 1);
 
-        // Assert that the email was received
-        MimeMessage[] receivedMessages = greenMail.getReceivedMessages();
-        Assert.assertEquals(1, receivedMessages.length);
+        // Retrieve the captured emails from the mock SMTP server
+        MimeMessage[] receivedMessages = smtpServer.getReceivedMessages();
+
+        // Assert the number of captured emails
+        assertEquals(3, receivedMessages.length);
+
+        // Assert the content of the captured email
+        MimeMessage receivedMessage = receivedMessages[0];
+        assertEquals("Test Email", receivedMessage.getSubject());
+        assertEquals("sender@example.com", receivedMessage.getFrom()[0].toString());
+        assertEquals("recipient@example.com", receivedMessage.getAllRecipients()[0].toString());
     }
 }
 

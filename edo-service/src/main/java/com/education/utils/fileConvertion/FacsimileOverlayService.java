52bcb31e0666536390a1ba2d4c4cedc60bf9549a
Yaroslav Ryabokon <ryabokon.spb@gmail.com>
2023-07-30 18:59:16 +0300

    add test overlay method with PDF facsimile

--- edo-service/src/main/java/com/education/utils/fileConvertion/FacsimileOverlayService.java
+++ edo-service/src/main/java/com/education/utils/fileConvertion/FacsimileOverlayService.java
@@ -1,6 +1,5 @@
 package com.education.utils.fileConvertion;
 
-import java.util.Map;
 
 public interface FacsimileOverlayService {
     byte[] overlay(byte[] bytes);
--- edo-service/src/main/java/com/education/utils/fileConvertion/impl/FacsimileOverlayServiceImpl.java
+++ edo-service/src/main/java/com/education/utils/fileConvertion/impl/FacsimileOverlayServiceImpl.java
@@ -7,9 +7,9 @@ import org.apache.pdfbox.multipdf.Overlay;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.common.PDStream;
 import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
@@ -28,33 +28,26 @@ public class FacsimileOverlayServiceImpl implements FacsimileOverlayService {
              ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
 
             PDDocument facsimile = PDDocument.load(new File("FACSIMILE.pdf"));
-
-            PDDocument overlayDoc = new PDDocument();
-            PDPage page = new PDPage();
-            overlayDoc.addPage(page);
-            Overlay overlayObj = new Overlay();
-            PDFont font = PDType1Font.COURIER_OBLIQUE;
-
-            PDPageContentStream contentStream = new PDPageContentStream(overlayDoc, page);
-            contentStream.setFont(font, 50);
-            contentStream.setNonStrokingColor(0);
-            contentStream.beginText();
-            contentStream.moveTextPositionByAmount(200, 200);
-            contentStream.drawString("deprecated");  // deprecated. Use showText(String text)
-            contentStream.endText();
-            contentStream.close();
-
             PDDocument originalDoc = PDDocument.load(bis);
-            overlayObj.setOverlayPosition(Overlay.Position.FOREGROUND);
-            overlayObj.setInputPDF(originalDoc);
-            overlayObj.setAllPagesOverlayPDF(overlayDoc);
-            Map<Integer, String> ovmap = new HashMap<Integer, String>(); // empty map is a dummy
-//            overlayObj.setOutputFile("... result-with-overlay.pdf");
-            overlayObj.overlay(ovmap);
-            overlayDoc.close();
+            Overlay overlayObj = new Overlay();
+//            PDPage page = originalDoc.getPage(0);
+
+
+//            PDImageXObject pdImage = PDImageXObject.createFromFile("FACSIMILE_3.png", originalDoc);
+//            PDPageContentStream contentStream = new PDPageContentStream(originalDoc, page);
+//            contentStream.drawImage(pdImage, 70, 140);
+//            contentStream.close();
+//            originalDoc.save(baos);
 //            originalDoc.close();
 
+            overlayObj.setOverlayPosition(Overlay.Position.FOREGROUND);
+            overlayObj.setInputPDF(originalDoc);
+            overlayObj.setAllPagesOverlayPDF(facsimile);
+            Map<Integer, String> ovmap = new HashMap<>(); // empty map is a dummy
+            overlayObj.overlay(ovmap);
             originalDoc.save(baos);
+
+            facsimile.close();
             originalDoc.close();
 
             return baos.toByteArray();

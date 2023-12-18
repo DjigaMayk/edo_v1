36f1fcd94b344fdb1637b6d65f326cdb8bb89d51
Seyran Akluntc <sseriann@mail.ru>
2023-12-18 18:46:15 +0300

    добавил AuthorMapper
    Создал метод findAuthorByFIO

--- edo-repository/src/main/java/com/education/controller/author/AuthorController.java
+++ edo-repository/src/main/java/com/education/controller/author/AuthorController.java
@@ -1,25 +1,36 @@
 package com.education.controller.author;
 
+import com.education.Utils.QuestionUtil;
 import com.education.entity.Author;
 import com.education.model.dto.AuthorDto;
 import com.education.service.author.AuthorService;
+import com.education.util.Mapper.impl.AuthorMapper;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
-import lombok.extern.log4j.Log4j2;
+import lombok.extern.java.Log;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.logging.Level;
 
 @RestController
 @AllArgsConstructor
+@Log
 @RequestMapping("api/repository/author")
-@Log4j2
 @Tag(name = "Контроллер Автора", description = "Контроллер Автора для выполнения веб-запросов (модуль edo-repository)")
 public class AuthorController {
 
     private final AuthorService authorService;
+
+    private final AuthorMapper mapper;
 
     @Operation(summary = "Поиск сущности по id")
     @GetMapping("/{id}")
@@ -51,6 +62,24 @@ public class AuthorController {
         authorService.delete(id);
         log.info("удалил AuthorDto.class");
         return ResponseEntity.ok("DELETED");
+    }
+
+    @ApiOperation("Поиск Автора по ФИО")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Author was successfully found"),
+            @ApiResponse(code = 404, message = "Author was not found")})
+    @GetMapping("/byFIO/")
+    public ResponseEntity<List<AuthorDto>> findAuthorByFIO(@RequestParam(value = "fio", required = false) String fio) {
+        String transformFio = QuestionUtil.textTransformer(fio);
+        String decodedFio = URLDecoder.decode(transformFio, StandardCharsets.UTF_8);
+        log.log(Level.INFO, "Получен запрос на поиск сущностей {0}", decodedFio);
+        List<Author> dtos = authorService.findAuthorByFIO(decodedFio);
+        List<AuthorDto> listDTO = mapper.toDto(dtos);
+        log.log(!listDTO.isEmpty()
+                        ? Level.INFO
+                        : Level.WARNING
+                , "Результат поиска сущностей: {0}", listDTO);
+        return new ResponseEntity<>(listDTO
+                , !listDTO.isEmpty() ? HttpStatus.OK : HttpStatus.NOT_FOUND);
     }
 
 }

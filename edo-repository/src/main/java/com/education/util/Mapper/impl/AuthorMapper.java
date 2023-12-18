427d5c3c91be00e6d315db66ac41a85b0cf50717
Seyran Akluntc <sseriann@mail.ru>
2023-12-18 18:48:15 +0300

    переопределил методы toDto и toEntity

--- edo-repository/src/main/java/com/education/util/Mapper/impl/AuthorMapper.java
+++ edo-repository/src/main/java/com/education/util/Mapper/impl/AuthorMapper.java
@@ -7,4 +7,10 @@ import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface AuthorMapper extends Mappable<Author, AuthorDto> {
+
+    @Override
+    AuthorDto toDto(Author author);
+
+    @Override
+    Author toEntity(AuthorDto authorDto);
 }

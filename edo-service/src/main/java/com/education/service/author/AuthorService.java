7a4ba6f4f0aa506149e37ae4b761f7f74b9fee82
Seyran Akluntc <sseriann@mail.ru>
2023-12-24 23:10:18 +0300

    добавил в AuthorService "service" метод findAuthorByFIO

--- edo-service/src/main/java/com/education/service/author/AuthorService.java
+++ edo-service/src/main/java/com/education/service/author/AuthorService.java
@@ -1,4 +1,4 @@
-package com.education.author_feign.service;
+package com.education.service.author;
 
 import com.education.model.dto.AuthorDto;
 import org.springframework.http.ResponseEntity;
@@ -14,5 +14,6 @@ public interface AuthorService {
     ResponseEntity<AuthorDto> save(AuthorDto author);
     ResponseEntity<String> delete(Long id);
     AuthorDto findById(Long id);
-    List<AuthorDto> findAllById(List<String> id);
+    List<AuthorDto> findAllById(List<Long> id);
+    List<AuthorDto> findAuthorByFIO(String fio);
 }

f4978a91d201771941c749dd9645bd0262edb88e
Seyran Akluntc <sseriann@mail.ru>
2023-12-24 23:06:26 +0300

    правки - изменил AuthorDro на Author в repository

--- edo-repository/src/main/java/com/education/repository/AuthorRepository.java
+++ edo-repository/src/main/java/com/education/repository/AuthorRepository.java
@@ -1,7 +1,6 @@
-package com.education.repository.author;
+package com.education.repository;
 
 import com.education.entity.Author;
-import com.education.model.dto.AuthorDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -18,5 +17,5 @@ public interface AuthorRepository extends JpaRepository<Author, Long>{
     @Query("SELECT aut FROM Author aut " +
             "WHERE LOWER(concat(aut.lastName,' ', aut.firstName,' ', aut.middleName)) " +
             "LIKE concat(LOWER(:fio) , '%') ORDER BY aut.lastName")
-    List<AuthorDto> findAuthorByFIO(@Param("fio") String fio);
+    List<Author> findAuthorByFIO(@Param("fio") String fio);
 }

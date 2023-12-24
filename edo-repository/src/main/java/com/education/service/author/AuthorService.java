f936bbb71406577526ece634ed9bbe506692da41
Seyran Akluntc <sseriann@mail.ru>
2023-12-24 23:08:52 +0300

    правки - изменил Author на AuthorDto в service

--- edo-repository/src/main/java/com/education/service/author/AuthorService.java
+++ edo-repository/src/main/java/com/education/service/author/AuthorService.java
@@ -1,6 +1,5 @@
 package com.education.service.author;
 
-import com.education.entity.Author;
 import com.education.model.dto.AuthorDto;
 
 import java.util.List;
@@ -15,7 +14,7 @@ public interface AuthorService {
     // save, delete, findById, findAllById
     AuthorDto save(AuthorDto author);
     void delete(Long id);
-    Author findById(Long id);
+    AuthorDto findById(Long id);
     List<AuthorDto> findAllById(List<Long> id);
     List<AuthorDto> findAuthorByFIO(String fio);
 }

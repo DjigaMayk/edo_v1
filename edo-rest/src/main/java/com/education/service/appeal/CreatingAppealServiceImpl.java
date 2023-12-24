11e68250ca2d603e2b9b50a793d2ea1a5b70f3b3
Seyran Akluntc <sseriann@mail.ru>
2023-12-24 23:16:06 +0300

    правки

--- edo-rest/src/main/java/com/education/service/appeal/CreatingAppealServiceImpl.java
+++ edo-rest/src/main/java/com/education/service/appeal/CreatingAppealServiceImpl.java
@@ -1,4 +1,4 @@
-package com.education.service.Appeal;
+package com.education.service.appeal;
 
 import com.education.feign.feign_appeal.AppealFeignClient;
 import com.education.model.dto.AppealAbbreviatedDto;

e12efbfca3083ede9f91c012ace29ee80cf28f10
Маслов Денис <maslovden22@yandex.ru>
2024-02-06 13:52:14 +0300

    feat: add methods for delete file, searching for old files (LastModified)

--- edo-file-storage/src/main/java/com/education/service/impl/MinioServiceImpl.java
+++ edo-file-storage/src/main/java/com/education/service/impl/MinioServiceImpl.java
@@ -1,9 +1,8 @@
 package com.education.service.impl;
 
 import com.education.service.MinioService;
-import io.minio.GetObjectArgs;
-import io.minio.MinioClient;
-import io.minio.PutObjectArgs;
+import io.minio.*;
+import io.minio.messages.Item;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +10,8 @@ import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.time.ZonedDateTime;
+import java.util.Iterator;
 import java.util.UUID;
 
 @Slf4j
@@ -55,6 +56,45 @@ public class MinioServiceImpl implements MinioService {
         }
 
         return stream;
+    }
+
+    @Override
+    public void deleteFileInBucketWithName(String bucketName, String fileName) {
+        log.info("Запрос на удаление объекта из MinIO. Баскет: " + bucketName + "; Имя объекта: " + fileName);
+        RemoveObjectArgs removeObjectArgs = RemoveObjectArgs.builder()
+                .bucket(bucketName)
+                .object(fileName)
+                .build();
+        try {
+            minioClient.removeObject(removeObjectArgs);
+            log.info("Объект удален");
+        } catch (Exception e) {
+            log.error("Ошибка при удалении объекта из MinIO: ", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void deleteFileWhoseRequestsMoreThanFiveYearsAgo() {
+        ZonedDateTime fiveYearsAgo = ZonedDateTime.now().minusYears(5);
+        log.info("Начало процедуры проверки объектов из MinIO на старость (последний запрос был более 5 лет).");
+        ListObjectsArgs listObjectsArgs = ListObjectsArgs.builder()
+                .bucket(bucketName)
+                .build();
+        Iterable<Result<Item>> listObjects = minioClient.listObjects(listObjectsArgs);
+        Iterator<Result<Item>> iterator = listObjects.iterator();
+        while (iterator.hasNext()) {
+            try {
+                Item item = iterator.next().get();
+                log.info("Объект " + item.objectName() + ". Последний запрос к объекту: " + item.lastModified());
+                if (item.lastModified().isBefore(fiveYearsAgo)) {
+                    deleteFileInBucketWithName(bucketName, item.objectName());
+                }
+            } catch (Exception e) {
+                log.error("Ошибка при получении объекта из MinIO: ", e);
+                throw new RuntimeException(e);
+            }
+        }
     }
 
 }
